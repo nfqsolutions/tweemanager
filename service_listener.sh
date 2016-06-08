9e6634ba8164c5a2f3131db42d04a2be440e1e1f
marrao <marrao@gmail.com>
2016-06-08 09:02:09 +0200

    Removing un needed files

--- service_listener.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pgrep -f "python tweemanager listener"
-if [[ $? -ne 0 ]] ; then
-        echo "Restarting service:     $(date)" >> /var/tmp/py_twee_man.log
-        .env/bin/python tweemanager listener &
-fi
