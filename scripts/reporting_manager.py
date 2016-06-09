346763f36fd095acac673519279296a6cee315b2
NFQ Solutions <nfq@localhost.localdomain>
2016-06-09 14:16:30 +0200

    Solved the accumulative problem r Please enter the commit message for your changes. Lines starting

--- scripts/reporting_manager.py
+++ scripts/reporting_manager.py
@@ -7,6 +7,14 @@ import datetime
 def genListOfdays(StartDate, EndDate=datetime.datetime.now()):
     """
     """
+    # onedaydelta = datetime.timedelta(days=1)
+    # StartDate = datetime.datetime(StartDate.year, StartDate.month, StartDate.day)
+    # EndDate = datetime.datetime(EndDate.year, EndDate.month, EndDate.day)
+    # while StartDate < EndDate:
+    #     yield {'Start': StartDate, 'End': StartDate + onedaydelta}
+    #     StartDate += onedaydelta
+
+    # Old Code
     onedaydelta = datetime.timedelta(days=1)
     StartDate = datetime.datetime(StartDate.year, StartDate.month, StartDate.day)
     EndDate = datetime.datetime(EndDate.year, EndDate.month, EndDate.day)
@@ -20,6 +28,17 @@ def genListOfdays(StartDate, EndDate=datetime.datetime.now()):
 def genListOfWeeks(StartDate, EndDate):
     """
     """
+    # onedaydelta = datetime.timedelta(days=1)
+    # oneweekdelta = datetime.timedelta(days=7)
+    # StartDate = datetime.datetime(StartDate.year, StartDate.month, StartDate.day)
+    # while StartDate.weekday() != 0:
+    #     StartDate = StartDate - onedaydelta
+    # EndDate = datetime.datetime(EndDate.year, EndDate.month, EndDate.day) + onedaydelta
+    # while StartDate < EndDate:
+    #     yield {'Start': StartDate, 'End': StartDate + oneweekdelta}
+    #     StartDate += oneweekdelta
+
+    # Old Code
     onedaydelta = datetime.timedelta(days=1)
     oneweekdelta = datetime.timedelta(days=7)
     StartDate = datetime.datetime(StartDate.year, StartDate.month, StartDate.day)
@@ -50,10 +69,10 @@ def aggCount(StartDate, EndDate):
     It returns the number of tweets between a time interval
     """
     pipeline = [
-        {"$match": {"created_at": {"$gte": StartDate}, "created_at": {"$lt": EndDate}}},
+        {"$match": {"$and": [{"created_at": {"$gte": StartDate}}, {"created_at": {"$lt": EndDate}}]}},
         {"$group": {"_id": "", "count": {"$sum": 1}}},
     ]
-
+    result = {'count':0}
     for result in coll.aggregate(pipeline):
         result = result
     return result
@@ -63,10 +82,11 @@ def aggPositivos(StartDate, EndDate):
     It returns the number of positive tweets classified by an algoritm in a time interval
     """
     pipeline = [
-        {"$match": {"created_at": {"$gte": StartDate}, "created_at": {"$lt": EndDate}}},
-        {"$group": {"_id": "", "npos": {"$sum": {"$cond":[{"$eq":["$valoration.algoritm.clasificado","positivo"]},1,0]}}
+        {"$match": {"$and": [{"created_at": {"$gte": StartDate}}, {"created_at": {"$lt": EndDate}}]}},
+        {"$group": {"_id": "", "npos": {"$sum": {"$cond":[{"$eq":["$valoration.algoritmo_7.clasificado","positivo"]},1,0]}}
         }},
     ]
+    result = {'npos':0}
     for result in coll.aggregate(pipeline):
         result = result
     return result
@@ -76,10 +96,11 @@ def aggNegativos(StartDate, EndDate):
     It returns the number of negative tweets classified by an algoritm in a time interval
     """
     pipeline = [
-        {"$match": {"created_at": {"$gte": StartDate}, "created_at": {"$lt": EndDate}}},
-        {"$group": {"_id": "", "nneg": {"$sum": {"$cond":[{"$eq":["$valoration.algoritm.clasificado","negativo"]},1,0]}}
+        {"$match": {"$and": [{"created_at": {"$gte": StartDate}}, {"created_at": {"$lt": EndDate}}]}},
+        {"$group": {"_id": "", "nneg": {"$sum": {"$cond":[{"$eq":["$valoration.algoritmo_7.clasificado","negativo"]},1,0]}}
         }},
     ]
+    result = {'nneg':0}
     for result in coll.aggregate(pipeline):
         result = result
     return result
@@ -89,18 +110,18 @@ def aggRepo(StartDate, EndDate):
     It returns another features of our classifier
     """
     pipeline = [
-        {"$match": {"created_at": {"$gte": StartDate}, "created_at": {"$lt": EndDate}}},
-        {"$group": {"_id": "", "count": {"$sum": "$valoration.algoritm.other"}
+        {"$match": {"$and": [{"created_at": {"$gte": StartDate}}, {"created_at": {"$lt": EndDate}}]}},
+        {"$group": {"_id": "", "count": {"$sum": "$valoration.algoritmo_7.dis_pos"}
         }},
         ]
+    result = {'count':0}
     for result in coll.aggregate(pipeline):
         result = result
     return result
 
 if __name__ == '__main__':
     #
-    your_connection = 'mongodb://127.0.0.1'
-    client = pymongo.MongoClient(your_connection, 27017)
+    client = pymongo.MongoClient('mongodb://127.0.0.1', 27017)
     db = client.tweets
     coll = db.TweetsRepo
     #
@@ -117,38 +138,65 @@ if __name__ == '__main__':
 
     # Perform aggregations on the time table:
     # 1º ListOfdays:
+    print("Por días")
     for values in genListOfdays(StartDate, EndDate):
         print("Tweets")
 
-        print(values,aggCount(values['Start'], values['End']))
+        print(values['Start'].strftime("%Y-%m-%d"),
+            values['End'].strftime("%Y-%m-%d"),
+            aggCount(values['Start'], values['End'])['count'])
         print("Positivos")
-        print(values,aggPositivos(values['Start'], values['End']))
+        print(values['Start'].strftime("%Y-%m-%d"),
+            values['End'].strftime("%Y-%m-%d"),
+            aggPositivos(values['Start'], values['End'])['npos'])
         print("Negativos")
-        print(values,aggNegativos(values['Start'], values['End']))
+        print(values['Start'].strftime("%Y-%m-%d"),
+            values['End'].strftime("%Y-%m-%d"),
+            aggNegativos(values['Start'], values['End'])['nneg'])
         print("Métrica")
-        print(values,aggRepo(values['Start'], values['End']))
+        print(values['Start'].strftime("%Y-%m-%d"),
+            values['End'].strftime("%Y-%m-%d"),
+            aggRepo(values['Start'], values['End'])['count'])
         print("")
 
     # 2º ListOfWeeks:
+    print("Por semanas")
     for values in genListOfWeeks(StartDate, EndDate):
         print("Tweets")
-        print(values,aggCount(values['Start'], values['End']))
+        print(values['Start'].strftime("%Y-%m-%d"),
+            values['End'].strftime("%Y-%m-%d"),
+            aggCount(values['Start'], values['End'])['count'])
         print("Positivos")
-        print(values,aggPositivos(values['Start'], values['End']))
+        print(values['Start'].strftime("%Y-%m-%d"),
+            values['End'].strftime("%Y-%m-%d"),
+            aggPositivos(values['Start'], values['End'])['npos'])
         print("Negativos")
-        print(values,aggNegativos(values['Start'], values['End']))
+        print(values['Start'].strftime("%Y-%m-%d"),
+            values['End'].strftime("%Y-%m-%d"),
+            aggNegativos(values['Start'], values['End'])['nneg'])
         print("Métrica")
-        print(values,aggRepo(values['Start'], values['End']))
+        print(values['Start'].strftime("%Y-%m-%d"),
+            values['End'].strftime("%Y-%m-%d"),
+            aggRepo(values['Start'], values['End'])['count'])
         print("")
 
     # 3º ListOfMonth:
+    print("Por meses")
     for values in genListOfMonth(StartDate, EndDate):
         print("Tweets")
-        print(values,aggCount(values['Start'], values['End']))
+        print(values['Start'].strftime("%Y-%m-%d"),
+            values['End'].strftime("%Y-%m-%d"),
+            aggCount(values['Start'], values['End'])['count'])
         print("Positivos")
-        print(values,aggPositivos(values['Start'], values['End']))
+        print(values['Start'].strftime("%Y-%m-%d"),
+            values['End'].strftime("%Y-%m-%d"),
+            aggPositivos(values['Start'], values['End'])['npos'])
         print("Negativos")
-        print(values,aggNegativos(values['Start'], values['End']))
+        print(values['Start'].strftime("%Y-%m-%d"),
+            values['End'].strftime("%Y-%m-%d"),
+            aggNegativos(values['Start'], values['End'])['nneg'])
         print("Métrica")
-        print(values,aggRepo(values['Start'], values['End']))
+        print(values['Start'].strftime("%Y-%m-%d"),
+            values['End'].strftime("%Y-%m-%d"),
+            aggRepo(values['Start'], values['End'])['count'])
         print("")
