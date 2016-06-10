d782f0c798bef9e6bfb53670fbca9e94b052d382
cperales <sir.perales@gmail.com>
2016-06-10 11:48:06 +0200

    A new function has been implemented. This function upload reporting to MongoDB

--- scripts/reporting_manager.py
+++ scripts/reporting_manager.py
@@ -3,6 +3,12 @@ import pymongo
 import datetime
 import json
 
+# IP host should be changed to your MongoDB server
+client = pymongo.MongoClient('mongodb://192.168.80.221:27017/')
+db = client.tweets
+coll = db.TweetsRepo
+name_collection = coll.name
+
 # 1º ListOfdays:
 def genListOfdays(StartDate, EndDate=datetime.datetime.now()):
     """
@@ -64,6 +70,7 @@ def aggCount(StartDate, EndDate):
         result = result
     return result
 
+
 # Reports to a JSON file
 def report_to_JSON(StartDate, EndDate):
     outfile = open('report_days.json', 'w')
@@ -111,14 +118,58 @@ def report_to_JSON(StartDate, EndDate):
         outfile.write("\n")
     outfile.close()
 
+# Reports to MongoDB
+def report_to_Mongo(StartDate, EndDate, name_collection):
+    coll = db.Reporting
+    
+    # 1º ListOfDays:
+    print("Uploading report by days...")
+    for values in genListOfdays(StartDate, EndDate):
+        linea = {}
+        valor = aggCount(values['Start'], values['End'])
+        linea['Start'] = values['Start']
+        linea['End'] = values['End']
+        linea['Tweets'] = valor['count']
+        linea['positives'] = valor['npos']
+        linea['negatives'] = valor['nneg']
+        linea['Metrica'] = valor['met']
+        linea['Report'] = {'type':'daily', 'from':name_collection}
+        key = values['Start'].strftime("%Y%m%d") + values['End'].strftime("%Y%m%d")
+        coll.update({"_id":key}, linea, upsert = True)
+
+    # 2º ListOfWeeks:
+    print("Uploading report by weeks...")
+    for values in genListOfWeeks(StartDate, EndDate):
+        linea = {}
+        valor = aggCount(values['Start'], values['End'])
+        linea['Start'] = values['Start']
+        linea['End'] = values['End']
+        linea['Tweets'] = valor['count']
+        linea['positives'] = valor['npos']
+        linea['negatives'] = valor['nneg']
+        linea['Metrica'] = valor['met']
+        linea['Report'] = {'type':'weekly', 'from':name_collection}
+        key = values['Start'].strftime("%Y%m%d") + values['End'].strftime("%Y%m%d")
+        coll.update({"_id":key}, linea, upsert = True)
+
+    # 3º ListOfMonth:
+    print("Uploading report by monts...")
+    for values in genListOfMonth(StartDate, EndDate):
+        linea = {}
+        valor = aggCount(values['Start'], values['End'])
+        linea['Start'] = values['Start']
+        linea['End'] = values['End']
+        linea['Tweets'] = valor['count']
+        linea['positives'] = valor['npos']
+        linea['negatives'] = valor['nneg']
+        linea['Metrica'] = valor['met']
+        linea['Report'] = {'type':'monthly', 'from':name_collection}
+        key = values['Start'].strftime("%Y%m%d") + values['End'].strftime("%Y%m%d")
+        coll.update({"_id":key}, linea, upsert = True)
+
 
 if __name__ == '__main__':
-    # You should change this IP to your MongoDB server
-    client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
-    db = client.tweets
-    coll = db.TweetsRepo
-    #
-
+    ## Example
     # 1º get first date
     for tweet in coll.find().sort('created_at', 1).limit(1):
         StartDate = tweet['created_at']
@@ -130,6 +181,8 @@ if __name__ == '__main__':
     print(EndDate)
 
     # 3º get all data:
+    # Uploading to MongoDB
+    report_to_Mongo(StartDate, EndDate, name_collection)
 
     # Write the reporting in a JSON file
     report_to_JSON(StartDate, EndDate)
@@ -138,7 +191,6 @@ if __name__ == '__main__':
     # 1º ListOfdays:
     print("Por días")
     for values in genListOfdays(StartDate, EndDate):
-        linea = {}
         valor = aggCount(values['Start'], values['End'])
         print("Tweets")
         print(values['Start'].strftime("%Y-%m-%d"),
@@ -149,6 +201,7 @@ if __name__ == '__main__':
     # 2º ListOfWeeks:
     print("Por semanas")
     for values in genListOfWeeks(StartDate, EndDate):
+        valor = aggCount(values['Start'], values['End'])
         print("Tweets")
         print(values['Start'].strftime("%Y-%m-%d"),
               values['End'].strftime("%Y-%m-%d"))
@@ -158,6 +211,7 @@ if __name__ == '__main__':
     # 3º ListOfMonth:
     print("Por meses")
     for values in genListOfMonth(StartDate, EndDate):
+        valor = aggCount(values['Start'], values['End'])
         print("Tweets")
         print(values['Start'].strftime("%Y-%m-%d"),
               values['End'].strftime("%Y-%m-%d"))
