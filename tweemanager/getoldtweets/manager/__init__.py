ee689b8572877f57f37323fede6d556cd33dfc03
SergioPompa <spompag@gmail.com>
2016-11-02 11:06:17 +0100

    created landing page

--- tweemanager/getoldtweets/manager/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .TweetCriteria import TweetCriteria
-from .TweetManager import TweetManager
--- tweemanager/getoldtweets/models/Tweet.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-class Tweet(dict):
-
-    def __init__(self):
-        pass
